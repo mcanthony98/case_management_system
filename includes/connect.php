b1bbc1f6ff6677065c4637ad03b66891e349a9c6
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2023-03-19 11:12:45 +0300

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'u640333703_root');
 define('DB_PASSWORD', '36987412Mm.');
-define('DB_DATABASE', 'u640333703_case_management_system');
+define('DB_DATABASE', 'u640333703_case_managemen');
 
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
